634ca07d1bc601f4d10ceaca2e23a94725c71ec5
Yellow <yellowcode.books@gmail.com>
2025-06-26 14:13:15 +0700

    Added UITest for DetailScreen

--- app/src/androidTest/java/com/peterdanh/githubuserbrowser/presentation/screen/detail/DetailScreenTest.kt
+++ app/src/androidTest/java/com/peterdanh/githubuserbrowser/presentation/screen/detail/DetailScreenTest.kt
@@ -1,9 +1,8 @@
-package com.peterdanh.githubuserbrowser.presentation.screen.home
+package com.peterdanh.githubuserbrowser.presentation.screen.detail
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
@@ -11,29 +10,12 @@ import com.peterdanh.githubuserbrowser.presentation.MainActivity
 import org.junit.Rule
 import org.junit.Test
 
-class HomeScreenTest {
+class DetailScreenTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
-    fun homeScreen_showsLoadingThenUsers() {
-        composeTestRule.mainClock.autoAdvance = true
-        composeTestRule.waitUntil(timeoutMillis = 10000) {
-            composeTestRule
-                .onAllNodesWithText("GitHub Users")
-                .fetchSemanticsNodes().isNotEmpty()
-        }
-
-        composeTestRule.onNodeWithText("GitHub Users").assertIsDisplayed()
-
-        composeTestRule
-            .onAllNodes(hasText("https://github.com", substring = true))
-            .onFirst()
-            .assertIsDisplayed()
-    }
-
-    @Test
-    fun clickingUser_navigatesToDetailScreen() {
+    fun detailScreen_displaysUserDetailsCorrectly() {
         composeTestRule.waitUntil(timeoutMillis = 10000) {
             composeTestRule
                 .onAllNodes(hasText("https://github.com", substring = true))
@@ -45,12 +27,18 @@ class HomeScreenTest {
             .onFirst()
             .performClick()
 
-        composeTestRule.waitUntil(timeoutMillis = 3000) {
+        composeTestRule.waitUntil(timeoutMillis = 5000) {
             composeTestRule
                 .onAllNodes(hasText("Follower", substring = true))
                 .fetchSemanticsNodes().isNotEmpty()
         }
 
-        composeTestRule.onNodeWithText("Follower").assertIsDisplayed()
+        composeTestRule
+            .onAllNodes(hasText("https://github.com", substring = true))
+            .onFirst()
+            .assertIsDisplayed()
+
+        composeTestRule.onNodeWithText("Follower", substring = true).assertIsDisplayed()
+        composeTestRule.onNodeWithText("Following", substring = true).assertIsDisplayed()
     }
 }
